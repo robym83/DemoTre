f308310550e73cba1568c531cb5a3588f13a6e4a
robym83 <robymdrive@gmail.com>
2025-05-28 18:51:29 +0200

    fatto modifica a Helloworld

---
+++ src/main/java/com/demo/HelloWorldController.java
@@ -0,0 +1,33 @@
+package com.demo;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
+	// ------------------- Ricerca Per Codice ------------------------------------
+		@RequestMapping(value = "/ciao", method = RequestMethod.GET, produces = "application/json")
+		public ResponseEntity<String> helloWorld()  
+				 
+		{
+			System.out.println("****** dentro HelloWorld Controller   *******");
+			logger.info("dentro HelloWorld");
+			logger.debug("dentro DEBUG HelloWorld");
+	
+			return new ResponseEntity<String>("ciao mamma", HttpStatus.OK);
+		}
+
+
+}
